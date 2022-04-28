98f8a84c6126c0a6b83e36dbe73a6cb89138770f
lvfuming <lvfuming@outlook.com>
2022-04-28 15:31:46 +0800

    fix:引入ts,可省略后缀。

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
-import routes from './routes.ts'
+import routes from './routes'
 import App from './App.vue'
 const app =createApp(App)
 
