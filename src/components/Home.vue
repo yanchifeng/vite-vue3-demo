bad23bf6cc57cb1d242218a6626b0e7bc300c06d
lvfuming <lvfuming@outlook.com>
2023-02-26 22:12:53 +0800

    add dark and element-plus

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -7,13 +7,25 @@
  * @FilePath: \hello-vue3\src\components\Home.vue
 -->
 <template>
-  home
+  <div class="common-layout">
+    <el-container>
+      <el-header>Header<el-button type="primary" @click="toggleDark()">{{ isDark ? 'Dark' : 'Light' }}</el-button></el-header>
+      <el-container>
+        <el-aside width="200px">Aside</el-aside>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
 </template>
 
-<script>
-export default {
+<script setup lang="ts">
+import { useToggle } from '@vueuse/shared'
+import { useDark } from '@vueuse/core'
 
-}
+const isDark = useDark({
+  storageKey: 'vitepress-theme-appearance',
+})
+const toggleDark = useToggle(isDark)
 </script>
 
 <style>
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
+import ElementPlus from 'element-plus'
+// import "~/styles/element/index.scss";
+
+// import ElementPlus from "element-plus";
+// import all element css, uncommented next line
+import "element-plus/dist/index.css";
+
+// or use cdn, uncomment cdn link in `index.html`
+
+import "~/styles/index.scss";
+import 'uno.css'
+
+// If you want to use ElMessage, import it.
+import "element-plus/theme-chalk/src/message.scss"
+
 import routes from './routes'
 import App from './App.vue'
 const app =createApp(App)
+app.use(ElementPlus)
 
 const router = createRouter({
   history: createWebHistory(),
